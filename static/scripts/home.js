46b61ee5c63e62e19410dcbd3146e1a713970a6e
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-27 23:39:40 -0600

    feat: get action id when clicking

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,17 +1,17 @@
 const URL = "http://localhost:8080";
 
 $(document).ready(() => {
-  $.get(URL + "/users", (data, status) => {
+  $.get(URL + "/users", function (data, status) {
     console.info(`status: ${status}`);
     if (status != "success") {
       console.log("something went wrong...");
       return;
     }
     $("#title").text(data.title);
-    data.users.forEach((user) => {
+    data.users.forEach(function (user) {
       let txt = `
         <tr>
-          <td ${user.id}>${user.name}</td>
+          <td>${user.name}</td>
           <td>
             <button class="btn btn-warning mx-2 action" id="edit-${user.id}" href="#">edit</button>
             <button class="btn btn-danger mx-2 action" id="delete-${user.id}" href="#">delete</button>
@@ -20,9 +20,22 @@ $(document).ready(() => {
       `;
       $("#users-container").append(txt);
     });
-    // TODO: when clicking an action button, get the id
-    $(".action").click(() => {
-      console.log($(this));
+    $(".action").click(function () {
+      let btnId = $(this).closest("button").attr("id").split("-");
+      let action = btnId[0];
+      let id = btnId[1];
+      switch (action) {
+        case "edit":
+          console.log("this is editing");
+          break;
+        case "delete":
+          console.log("this is deleting lole");
+          break;
+        default:
+          console.log("error... guh??");
+          break;
+      }
+      console.log(action, id);
     });
   });
 });
